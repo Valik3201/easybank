a9ac4db298a7aea75c91d820bfc9727e146ec277
Valik <valik3201@gmail.com>
2023-09-02 19:30:24 +0200

    Add hero section

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,25 @@
 const btnHumburger = document.querySelector("#btnHumburger");
+const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+const fadeElements = document.querySelectorAll(".has-fade");
 
 btnHumburger.addEventListener("click", function () {
   if (header.classList.contains("open")) {
     // Close Humburger Menu
     header.classList.remove("open");
-    overlay.classList.remove("fade-in");
-    overlay.classList.add("fade-out");
+    body.classList.remove("noscroll");
+    fadeElements.forEach(function (element) {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
   } else {
     // Open Humburger Menu
+    body.classList.add("noscroll");
     header.classList.add("open");
-    overlay.classList.remove("fade-out");
-    overlay.classList.add("fade-in");
+    fadeElements.forEach(function (element) {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
   }
 });
