e72831ff3ba66e723f452c38fbb0753c1e279e53
Valik <valik3201@gmail.com>
2023-11-29 10:28:04 +0100

    Added script comments

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,6 @@
+/**
+ * Toggle the visibility of the hamburger menu and its associated elements.
+ */
 const btnHumburger = document.querySelector("#btnHumburger");
 const body = document.querySelector("body");
 const header = document.querySelector(".header");
@@ -6,17 +9,21 @@ const fadeElements = document.querySelectorAll(".has-fade");
 
 btnHumburger.addEventListener("click", function () {
   if (header.classList.contains("open")) {
-    // Close Humburger Menu
+    // Close Hamburger Menu
     header.classList.remove("open");
     body.classList.remove("noscroll");
+
+    // Remove fade-in and add fade-out to fade elements
     fadeElements.forEach(function (element) {
       element.classList.remove("fade-in");
       element.classList.add("fade-out");
     });
   } else {
-    // Open Humburger Menu
+    // Open Hamburger Menu
     body.classList.add("noscroll");
     header.classList.add("open");
+
+    // Remove fade-out and add fade-in to fade elements
     fadeElements.forEach(function (element) {
       element.classList.remove("fade-out");
       element.classList.add("fade-in");
